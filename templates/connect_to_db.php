3b74f6958b9e262057add820edcd31e4d40f33a8
Amir <amir@musaev.online>
2020-11-20 13:31:34 +0000

    fix: Change database to external

--- templates/connect_to_db.php
+++ templates/connect_to_db.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysqli_connect("localhost", "admin", "4r3r", "shop_v0");
+$db = mysqli_connect("sql7.freemysqlhosting.net", "sql7377743", "IrBVPZwQ5P", "sql7377743");
 $db->set_charset("utf8");
 ?>
